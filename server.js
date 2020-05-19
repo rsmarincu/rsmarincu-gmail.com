816c92196cd813b486687c9adbd3cc7fa0d15053
Robert <rsm43@bath.ac.uk>
2020-05-19 12:28:30 +0100

    Added express server

---
+++ server.js
@@ -0,0 +1,13 @@
+let express = require('express');
+let path = require('path');
+let serverStatic = require('serve-static');
+
+app = express();
+app.use(serverStatic(__dirname + '/dist'));
+
+let port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log('server started on port: ' + port);
+
+
