dd683db7cf33312a7c954fe0cf02e9cdecbb29c3
Robert <rsm43@bath.ac.uk>
2020-06-25 19:14:26 +0100

    Fixed the fix

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var serveStatic = require('serve-static');
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
 var port = process.env.PORT || 5000;
-var hostname = '127.0.0.1';
+var hostname = 'localhost';
 
 app.listen(port, hostname, () => {
    console.log(`Server running at http://${hostname}:${port}/`);
