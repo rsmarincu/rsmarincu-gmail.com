3e6a8b9fc4882cab3b23fcf744c72662b4461d62
Robert <rsmarincu@gmail.com>
2020-07-10 12:51:14 +0300

    Before pull

--- src/components/AddComponent.js
+++ src/components/AddComponent.js
@@ -2,6 +2,7 @@ import Rete from 'rete'
 import { NumControl } from '../controls/NumControl'
 import { numSocket } from '../sockets'
 import component from '../vue-components/Custom.vue';
+import Axios from 'axios';
 
 
 export class AddComponent extends Rete.Component {
@@ -22,10 +23,24 @@ export class AddComponent extends Rete.Component {
             .addOutput(out);
     }
 
-    worker(node, inputs, outputs) {
-        var n1 = inputs['addInp1'].length?inputs['addInp1'][0]:node.data.addInp1;
-        var n2 = inputs['addInp2'].length?inputs['addInp2'][0]:node.data.addInp2;
-        var sum = n1 + n2;
+    async worker(node, inputs, outputs) {
+        let n1 = inputs['addInp1'].length?inputs['addInp1'][0]:node.data.addInp1;
+        let n2 = inputs['addInp2'].length?inputs['addInp2'][0]:node.data.addInp2;
+        let sum = null;
+        try {
+            const resp = await Axios.get("localhost/math/add/", {
+                params: {
+                    'as': n1,
+                    'bs': n2  
+                }
+            })
+            sum = resp.data
+        }
+        catch (error) {
+            sum = null
+        }
+
+        
         
         this.editor.nodes.find(n => n.id == node.id).controls.get('preview').setValue(sum);
         outputs['addOut'] = sum;
