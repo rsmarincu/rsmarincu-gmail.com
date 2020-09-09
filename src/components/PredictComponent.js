2fdbaff96a3426e8d4dffc95cb21377574d4cab4
Robert <rsm43@bath.ac.uk>
2020-09-09 20:03:12 +0100

    Added preview for Select Dataset

--- src/components/PredictComponent.js
+++ src/components/PredictComponent.js
@@ -49,11 +49,6 @@ export class PredictComponent extends Rete.Component {
         let task = inputs['ttid'][0]
         let target = inputs['target'].flat()[0]
         let predict = inputs['predict'][0]
-
-        if (typeof predict === "string") {
-            predict = JSON.stringify(predict)
-        }
-
         let session_id = getRandomInt(1, 100000)
         let ttid = null
 
